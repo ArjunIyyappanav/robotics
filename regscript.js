7c3cf4cde87f5aae558e7681ef22033c75836d43
Arjun I <arjun.iyyappan.2005@gmail.com>
2024-12-27 13:54:17 +0530

    changed background,added login and animations

---
+++ regscript.js
@@ -0,0 +1,8 @@
+document.querySelector(".submitbtn").addEventLisenter("click",function(e){
+    alert("Submitted");
+    // document.querySelector("small").style.visibility="visible";
+});
+
+function submitted(){
+    document.querySelector("small").style.visibility="visible";
+}
